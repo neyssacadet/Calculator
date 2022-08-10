1b6a44a970124c1d2269ff8435968dd95b254fa7
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-08-09 21:03:31 -0500

    created functions for all basic math operators

---
+++ external.js
@@ -0,0 +1,22 @@
+function addNum (firstNum, nextNum){
+    let addition = firstNum + nextNum;
+    return addition;
+}
+console.log (addNum(3,2));
+
+function substractNum (firstNum, nextNum){
+    let substraction = firstNum - nextNum;
+    return substraction;
+}
+console.log(substractNum(9,8))
+
+function multiplyNum (firstNum, nextNum){
+    let multiplication = firstNum * nextNum; 
+    return multiplication;
+}
+
+function divideNum (firstNum, nextNum){
+    let division = firstNum/nextNum;
+    return division;
+}
+console.log(divideNum(15,5))
