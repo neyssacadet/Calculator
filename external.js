eba9859d2f76d7ef2711214b9e23d93826d9f252
Neyssa Cadet <neyssacadet2304@gmail.com>
2022-08-30 20:05:40 -0500

    create a set within a variable that groups all of the operators, equal and clear signs

--- external.js
+++ external.js
@@ -1,5 +1,5 @@
 let message = ""; 
-
+let operators = new Set (["add", "multiply", "substract", "divide"]);
 
 // functions for operating 2 numbers 
 function addNum (a, b){
@@ -21,49 +21,6 @@ function divideNum (a, b){
     return a/b;
 }
 console.log(divideNum(2,3));
-
-/*let operator = "";
-//function that takes an operator and 2 numbers
-function operate(operator, firstNum, nextNum){
-  if(operator = "+"){
-    addNum(firstNum, nextNum);
-  }
-  else{
-    alert("error");
-  }
-}
-
-
-function that takes an operator and 2 numbers and calls another function
-function operate (a, b){
-  switch(operator){
-    case "+":
-      addNum (a, b);
-    case "-":
-      substractNum (a, b);
-    case "*":
-      multiplyNum (a, b);
-    case "/":
-      divideNum (a, b);
-  }
-}
-
-function operate (operator, a, b){
-  if (operator == box.id){
-    addNum(a, b);
-  }
-  else if (operator == substract){
-    substractNum(a, b);
-  }
-  else if (operator == multiply){
-    multiplyNum(a, b);
-  }
-  else if (operator == divide){
-    divideNum(a, b);
-  }
- 
-}*/
-
 
 
 window.addEventListener("DOMContentLoaded", function() {
